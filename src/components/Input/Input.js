5a1316dd62e730d12c3f63699ba403d131444fc3
Cem <cmbicakci@gmail.com>
2022-08-18 21:25:52 +0300

    added input section

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './style.css'
 
 function Input() {
   return (
-    <div>Input</div>
+    <div className='input-container'>
+      <div className='input-item'>
+        <label>Paragraphs</label>
+        <input type='number' />
+      </div>
+
+      <div className='input-item'>
+        <label>Include HTML</label>
+        <select>
+          <option>Yes</option>
+          <option>No</option>
+        </select>
+      </div>
+    </div>
   )
 }
 
