cc114f73f38b43fe3f9acf76f4271dab0bfeed64
Cem <cmbicakci@gmail.com>
2022-08-18 20:12:40 +0300

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+function Header() {
+  return (
+    <header>
+        <h1>React sample text generator app</h1>
+    </header>
+  )
+}
+
+export default Header
