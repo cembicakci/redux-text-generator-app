ded082d2b1a52a5598053bef073f21c3ad7be563
Cem <cmbicakci@gmail.com>
2022-08-18 21:15:50 +0300

    added redux

---
+++ src/redux/textSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const textSlice = createSlice({
+    name: "text",
+    initialState: {},
+    reducers: {}
+})
+
+export default textSlice.reducer
